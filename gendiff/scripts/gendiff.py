0e40459700a022622af613367239bca943751602
RomanKalsin <karen52rus@gmail.com>
2021-10-11 16:52:58 +0300

    WORK: add first logic gendiff

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,4 +1,5 @@
 import argparse
+import json    
 
 
 def main():
@@ -7,6 +8,33 @@ def main():
     parser.add_argument('second_file')
     parser.add_argument('-f', '--format', default="JSON", help='set format of output')
     args = parser.parse_args()
+
+
+def generate_diff(file1, file2):
+    #/mnt/c/python/code/python-project-lvl2/file/file1.json
+    #C:\Python\Code\python-project-lvl2\\file\\file1.json
+    result = "{\n"
+    file1 = json.load(open('/mnt/c/python/code/python-project-lvl2/file/file1.json'))
+    file2 = json.load(open('/mnt/c/python/code/python-project-lvl2/file/file2.json'))
+    set_file1 = set(file1.keys())
+    set_file2 = set(file2.keys())
+    set_crossing = set_file1 & set_file2
+    set_only_file1 = set_file1 - set_file2
+    set_only_file2 = set_file2 - set_file1
+    final_key_order = sorted(set_file1 | set_file2)
+    for key in final_key_order:
+        if key in set_crossing:
+            if file1[key] == file2[key]:
+                result += '    {}: {}\n'.format(key, file1[key])
+            else:
+                result += '  - {}: {}\n'.format(key, file1[key])
+                result += '  + {}: {}\n'.format(key, file2[key])
+        elif key in set_only_file1:
+            result += '  - {}: {}\n'.format(key, file1[key])
+        elif key in set_only_file2:
+            result += '  + {}: {}\n'.format(key, file2[key])
+    result += '}'
+    return result
 
 
 if __name__ == "__main__":
