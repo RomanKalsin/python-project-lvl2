ac96ebcb7da8f6dcd304ecf7b6c339c42184ccdd
RomanKalsin <karen52rus@gmail.com>
2021-10-17 19:53:15 +0300

    HOME: restructuring project

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,6 +1,6 @@
 from gendiff.logic import generate
 
 
-def generate_diff(source1, source2, name):
+def generate_diff(source1, source2, name='default'):
     diff = generate(source1, source2)
     return name(diff)
